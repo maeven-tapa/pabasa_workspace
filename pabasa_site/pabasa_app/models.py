dd45bb9630e59091e3333c9d37964164e339782a
AmielJohnPadasay <amieljohn.padasay@gsfe.tupcavite.edu.ph>
2026-06-04 18:42:53 +0800

    Start of implementing backend/database
    - Added new "users" table
    - Starting to edit the models.py

--- pabasa_site/pabasa_app/models.py
+++ pabasa_site/pabasa_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    ROLE_CHOICES = [
+        ("teacher", "Teacher"),
+        ("student", "Student"),
+    ]
+
+    id = models.BigAutoField(primary_key=True)
+    custom_id = models.CharField(max_length=20, unique=True, editable=False)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    sex = models.CharField(max_length=10)
+    birth_month = models.PositiveSmallIntegerField()
+    birth_day = models.PositiveSmallIntegerField()
+    birth_year = models.PositiveSmallIntegerField()
+    email = models.EmailField(unique=True)
+    contact_no = models.CharField(max_length=20, blank=True, null=True)
+    password_hash = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
