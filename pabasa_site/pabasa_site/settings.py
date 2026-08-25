8e70e283f720f07f3c328065732008e25b056efa
maeven-tapa <maeven.tapa@gsfe.tupcavite.edu.ph>
2026-08-26 04:26:54 +0800

    Configure Speech-to-Text project

--- pabasa_site/pabasa_site/settings.py
+++ pabasa_site/pabasa_site/settings.py
@@ -165,7 +165,7 @@ DEFAULT_FROM_EMAIL = 'pabasa.tupc@gmail.com'
 
 # Google Cloud Speech-to-Text settings used by the reading assessment UI.
 GOOGLE_STT_API_KEY = 'AIzaSyDPMgJX8t195Z9bfoID2gwlG5oPhBVK_tk'
-GOOGLE_CLOUD_PROJECT_ID = 'direct-outlet-499701-p6'
+GOOGLE_CLOUD_PROJECT_ID = 'project-2b0d295d-ee06-40a7-927'
 GOOGLE_STT_LOCATION = 'us'
 # Chirp 3 is used through Speech-to-Text V2 StreamingRecognize for live reading.
 GOOGLE_STT_MODEL = 'chirp_3'
