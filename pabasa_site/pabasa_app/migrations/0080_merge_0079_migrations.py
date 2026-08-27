9597e92f53332cf4560d6fc56f91d75fef8e4f6f
maeven-tapa <maeven.tapa@gsfe.tupcavite.edu.ph>
2026-08-27 22:26:09 +0800

    Merge conflicting pabasa app migrations

---
+++ pabasa_site/pabasa_app/migrations/0080_merge_0079_migrations.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("pabasa_app", "0079_assessment_crla_score_data"),
+        ("pabasa_app", "0079_calendarevent_scope_school"),
+    ]
+
+    operations = []
