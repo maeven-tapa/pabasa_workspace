7a839433691a4ee13ae5b1ec9bbc4a472e83d568
Dona <donaflor.palacios@gsfe.tupcavite.edu.ph>
2026-09-02 18:47:31 +0800

    Create 0095_merge_0094_section_assessment_week_enabled_0094_teacher_aral_schedule.py

---
+++ pabasa_site/pabasa_app/migrations/0095_merge_0094_section_assessment_week_enabled_0094_teacher_aral_schedule.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("pabasa_app", "0094_section_assessment_week_enabled"),
+        ("pabasa_app", "0094_teacher_aral_schedule"),
+    ]
+
+    operations = []
