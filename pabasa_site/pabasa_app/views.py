2b27aaf5cf3ac3b7c70f8d2a49101bec20e58aa1
raeiyna <197688722+raeiyna@users.noreply.github.com>
2026-09-16 04:52:04 +0800

    Implement Session 4 Gawain 4 student activity

--- pabasa_site/pabasa_app/views.py
+++ pabasa_site/pabasa_app/views.py
@@ -13283,6 +13283,12 @@ def session_4_gawain_3_page(request):
 
 
 @login_required(role='student')
+@xframe_options_sameorigin
+def session_4_gawain_4_page(request):
+    return render(request, 'pabasa_app/session_4_gawain_4_page.html', _dashboard_context(request))
+
+
+@login_required(role='student')
 @csrf_protect
 @require_http_methods(['POST'])
 def lesson_3_activity_progress(request):
