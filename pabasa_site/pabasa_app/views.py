cb53987a4b365ef0f12c073f7980d74d54bd3c18
AmielJohnPadasay <amieljohn.padasay@gsfe.tupcavite.edu.ph>
2026-09-20 09:45:20 +0800

    Refine Gawain 4 for Lesson 16

--- pabasa_site/pabasa_app/views.py
+++ pabasa_site/pabasa_app/views.py
@@ -14043,6 +14043,7 @@ def prescribed_activity_page(request, activity_key):
                       for item in activity['items']],
             'progress_url': reverse('prescribed_activity_progress', kwargs={'activity_key': activity_key}),
             'completion_url': reverse('prescribed_activity_complete', kwargs={'activity_key': activity_key}),
+            'read_aloud_url': reverse('reading_read_aloud_api'),
             'progress': {'current_index': len(answers), 'completed_items': len(answers),
                          'correct_items': len(answers), 'total_items': len(activity['items']),
                          'activity_completed': progress.activity_completed if progress else False,
